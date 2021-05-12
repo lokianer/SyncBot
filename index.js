bdf5e22352c0e15e98453f89dc53b3340f0d6bb2
Lokianer <lokianer@gmail.com>
2021-05-12 13:54:47 +0200

    Added config.example.json and added come catches for the login

--- index.js
+++ index.js
@@ -3,33 +3,42 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 
 const twitch_client = new tmi.Client({
-    options: { debug: true },
-    connection: {
-      secure: true,
-      reconnect: true
-    },
-    identity: {
-      username: config.TWITCH_ACCOUNT,
-      password: config.TWITCH_TOKEN
-    },
-    channels: [config.TWITCH_CHANNEL]
-  });
-  
+  options: { debug: true },
+  connection: {
+    secure: true,
+    reconnect: true
+  },
+  identity: {
+    username: config.TWITCH_ACCOUNT,
+    password: config.TWITCH_TOKEN
+  },
+  channels: [config.TWITCH_CHANNEL]
+});
+
 
 const discord_client = new Discord.Client();
+
 discord_client.login(config.DISCORD_BOT_TOKEN)
+  .then((response) => {
+    console.log(`I am connected to a Discord-Server and will push messages to ${config.DISCORD_CHANNEL}`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 twitch_client.on('message', onMessageHandler);
-twitch_client.on('connected', onConnectedHandler);
 
-twitch_client.connect();
 
-function onMessageHandler (target, context, msg, self) {
-    if (self) { return; } // Ignore messages from the bot
-    discord_client.channels.cache.get(config.DISCORD_CHANNEL).send(`${context.username}: ${msg}`)
+twitch_client.connect()
+  .then((response) => {
+    console.log(`I am connected to the Twicth-Channel ${config.TWITCH_CHANNEL}`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-  }
-  
-  function onConnectedHandler (addr, port) {
-    console.log(`* Connected to ${addr}:${port}`);
-  }
+function onMessageHandler(target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+  discord_client.channels.cache.get(config.DISCORD_CHANNEL).send(`${context.username}: ${msg}`)
+
+};
